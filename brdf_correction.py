046018254a95c956b581cda4f85c9f638beaab40
Phil <pbrodrick@dge-mini-ciw.stanford.edu>
2019-07-24 15:56:15 -0700

    added basic matrix generation implementation

--- brdf_correction.py
+++ brdf_correction.py
@@ -4,11 +4,22 @@
 
 
 import numpy as np
+import argparse
+import gdal
+from tqdm import tqdm
+
 
 h_over_b = 2.0
 b_over_r = 10.0
 n_classes = 4
 n_bands = 426
+wv = np.array([383.68 + i*5 for i in range(426)])
+
+
+def get_ndvi(refl):
+    _860 = np.argmin(np.abs(np.array(wv) - 860))
+    _660 = np.argmin(np.abs(np.array(wv) - 860))
+    return (refl[:,_860] - refl[:,_660]) / (refl[:,_860] + refl[:,_660])
 
 def theta_prime(theta):
     """ Convert an angle to it's 'prime', (Eq 2 in Colgen, et al. 2012)
@@ -43,7 +54,7 @@ def correction_components(solar_zenith_deg, sensor_zenith_deg, sensor_azimuth_de
     return F_1, F_2
 
 def generate_coeff_table(refl, tch, shade, relobs):
-    roughclass = separate_classes(getndvi(refl), tch, shade)
+    roughclass = separate_classes(get_ndvi(refl), tch, shade)
     coeff_mat = np.zeros((n_classes, n_bands, 3))
 
     for _class in range(n_classes):
@@ -81,7 +92,7 @@ def apply_brdf_model(coef, refl, relobs, tch, shade):
 
     F_1, F_2 = correction_components(relobs[:,0], relobs[:,1], relobs[:,2], relobs[:,3])
     refF_1, refF_2 = correction_components(np.ones(relobs[:,0].shape)*40, np.zeros(relobs[:,0].shape), np.zeros(relobs[:,0].shape), np.zeros(relobs[:,0].shape))
-    roughclass = separate_classes(getndvi(refl), tch, shade)
+    roughclass = separate_classes(get_ndvi(refl), tch, shade)
 
 
     for _class in range(n_classes):
@@ -91,21 +102,54 @@ def apply_brdf_model(coef, refl, relobs, tch, shade):
             ref_r = coef[_class,_band,0] + refF_1[subset] * coef[_class,_band,1] * refF_2[subset]*coef[_class,_band,2]
             refl[subset,_band] = refl[subset,_band] * ref_r / modeled_r
 
-
     return refl
 
 
 
 
 
+parser = argparse.ArgumentParser(description='Perform BRDF correction on mosaic')
+parser.add_argument('reflectance_file')
+parser.add_argument('obs_file')
+parser.add_argument('tch_file')
+parser.add_argument('shade_file')
+args = parser.parse_args()
 
 
+# Step 1 - Build reference table
 
+refl_set = gdal.Open(args.reflectance_file,gdal.GA_ReadOnly)
+obs_set = gdal.Open(args.obs_file,gdal.GA_ReadOnly)
+tch_set = gdal.Open(args.tch_file,gdal.GA_ReadOnly)
+shade_set = gdal.Open(args.shade_file,gdal.GA_ReadOnly)
 
+perm = np.random.permutation((refl_set.RasterYSize,refl_set.RasterXSize))
+num_samples = 1000
+matrix_subset = perm <= num_samples
+del perm
 
+refl = np.zeros((num_samples, refl_set.RasterCount))
+obs = np.zeros((num_samples, obs_set.RasterCount))
+tch = np.zeros((num_samples, tch_set.RasterCount))
+shade = np.zeros((num_samples, shade_set.RasterCount))
 
+_ind = 0
+for _row in tqdm(range(refl_set.RasterYSize)):
+    if (np.sum(matrix_subset[_row,:]) > 0):
+        loc_tch = np.transpose(np.squeeze(tch_set.ReadAsArray(0,_row,tch_set.RasterXSize,1))[:, matrix_subset[_row]])
+        loc_refl = np.transpose(np.squeeze(refl_set.ReadAsArray(0,_row,refl_set.RasterXSize,1))[:, matrix_subset[_row,:]])
+        loc_shade = np.transpose(np.squeeze(shade_set.ReadAsArray(0,_row,shade_set.RasterXSize,1))[:, matrix_subset[_row]])
+        loc_obs = np.transpose(np.squeeze(obs_set.ReadAsArray(0,_row,obs_set.RasterXSize,1))[:, matrix_subset[_row,:]])
 
+        tch[_ind:_ind+len(loc_tch)] = loc_tch
+        shade[_ind:_ind+len(loc_tch)] = loc_shade
+        refl[_ind:_ind+len(loc_tch),:] = loc_refl
+        obs[_ind:_ind+len(loc_tch),:] = loc_obs
 
+        _ind += len(loc_tch)
+
+coeff_mat = generate_coeff_table(refl, tch, shade, obs[:,[4,2,1,3]])
+print(coeff_mat)
 
 
 
